ec2f48fb297fee6a05097293493ff53020fa7ab6
Balthazar Frolin <balthov60@gmail.com>
2020-09-30 16:13:45 +0100

    Initial Commit

---
+++ TriangleDemo/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  TriangleDemo
+//
+//  Created by Balthazar Frolin on 30/09/2020.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
